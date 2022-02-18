3d7c6467455adc87ef574755235dcbb9d1888837
Izaac Baptista <izaacbaptista@gmail.com>
2022-02-18 15:50:52 -0300

    aula 01

---
+++ aula01/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    printf("Aprendendo comentários na linguagem C \n");
+
+    printf("Hello World! \n");
+    //comentário
+    return 0;
+}
