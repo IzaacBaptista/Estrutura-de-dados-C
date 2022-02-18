f2da5dcffba8a14b85fbd36dfbad673b1e616246
Izaac Baptista <izaacbaptista@gmail.com>
2022-02-18 16:08:38 -0300

    Imprimindo informações com a função printf e caractere nova linha

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-### Curso-de-Programação-C
+# Curso-de-Programação-C
+
+## estruturas de dados
+
+## Pilhas
+
+## Algoritmos
+
+## Árvore
+
+## Lista
+
+## Array
+
+## Alocação de memória
---
+++ aula02/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\n--------------------------------------------------\n");
+    printf("1 - Logar      2 - Cadastrar      3 - Imprimir\n");
+    printf("\n--------------------------------------------------\n");
+
+    printf("\nValor retornado: %d", printf("Bomm"));
+
+    //%d retorna a quantidade de caracteres
+
+    return 0;
+}
