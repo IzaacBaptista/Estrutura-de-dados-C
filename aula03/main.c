2b771454ad4f14684f6ed3ad7b8aba0a4804a51e
Izaac Baptista <izaacbaptista@gmail.com>
2022-02-18 16:20:07 -0300

    Não use a função system ("pause")

---
+++ aula03/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\n--------------------------------------------------\n");
+    printf("1 - Logar      2 - Cadastrar      3 - Imprimir\n");
+    printf("\n--------------------------------------------------\n");
+
+    printf("\nValor retornado: %d", printf("Bomm"));
+
+    printf("\n Pressione enter para finalizar.");
+    getchar();
+
+    return 0;
+}
