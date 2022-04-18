354b0964c434d69847ea3e9ec99ca1a4e305bd97
Izaac Baptista <izaacbaptista@gmail.com>
2022-04-18 15:42:00 -0300

    Como usar o operador long para o tipo primitivo double

---
+++ aula20/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//Qual o tamanho em memória de um float?
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    
+
+    return 0;
+}
