452a896c0ad942fc8aaef26fb394f79d1abcd8f4
Izaac Baptista <izaacbaptista@gmail.com>
2022-04-18 16:00:52 -0300

    Como usar o operador long para o tipo primitivo double

---
+++ aula21/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+//Como usar o operador long para o tipo primitivo double?
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    
+
+    return 0;
+}
