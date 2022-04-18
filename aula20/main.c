7916e5d1cd5bde00441263acc64db5361f447421
Izaac Baptista <izaacbaptista@gmail.com>
2022-04-18 15:55:14 -0300

    Como usar o operador long para o tipo primitivo double

--- aula20/main.c
+++ aula20/main.c
@@ -12,8 +12,23 @@ int main(void)
 
     double y = 3.14155896587458962548;
 
-    printf("Valor de Y: %lf\n", y);
+    long double z = 3.14155896587458962548;
+
+    printf("Valor de X é: %lf\n", x);
+    printf("Valor de X é: %.10lf\n", x);
+    printf("Um float precisa de %d bytes de memoria.\n", sizeof x);
+    printf("Um float precisa de %d bytes de memoria.\n", sizeof(float));
+    printf("----------------------------------------------------------------------\n");
+    printf("Valor de Y é: %lf\n", y);
+    printf("Valor de Y é: %.10lf\n", y);
     printf("Um double precisa de %d bytes de memoria.\n", sizeof y);
+    printf("Um double precisa de %d bytes de memoria.\n", sizeof(double));
+    printf("-----------------------------------------------------------------------\n");
+
+    printf("Valor de Z é: %lf\n", z);
+    printf("Valor de Z é: %.10lf\n", z);
+    printf("Um long double precisa de %d bytes de memoria.\n", sizeof z);
+    printf("Um long double precisa de %d bytes de memoria.\n", sizeof(long double));
 
     return 0;
 }
