50bd685337c7eb41c95f6253c9b0347bb6076fe8
Izaac Baptista <izaacbaptista@gmail.com>
2022-04-25 14:36:50 -0300

    O que significa Casting ou conversão de tipos na linguagem C

---
+++ aula23/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+     O que significa Casting ou conversão de tipos na linguagem C
+*/
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    int a = 10, b = 20;
+
+    printf("\nSoma: %d\n", a + b);
+    printf("\nSubtração: %d\n", a - b);
+    printf("\nMultiplicação: %d\n", a * b);
+    printf("\nDivisão: %d\n", a / b);
+    printf("\nDivisão: %d\n", (float)a / b); //numeros inteiros
+    printf("\nDivisão: %f\n", a / b);
+    printf("\nDivisão: %f\n", (float)a / b); //numeros inteiros
+
+    return 0;
+}
