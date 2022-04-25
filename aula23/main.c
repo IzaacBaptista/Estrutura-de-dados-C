e9ac69b6df0f81c5d3629b78470ec2daa630390a
Izaac Baptista <izaacbaptista@gmail.com>
2022-04-25 14:38:47 -0300

    O que significa Casting ou conversão de tipos na linguagem C

--- aula23/main.c
+++ aula23/main.c
@@ -11,6 +11,11 @@ int main(void)
     setlocale(LC_ALL, "Portuguese");
 
     int a = 10, b = 20;
+    float pi = 3.1415;
+
+    a = (int)pi;
+
+    printf("\nO valor de a: %d\n", a)
 
     printf("\nSoma: %d\n", a + b);
     printf("\nSubtração: %d\n", a - b);
