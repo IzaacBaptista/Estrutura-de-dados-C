eb87e2b5a518d617efa4692f249ef74027a55440
Izaac Baptista <izaacbaptista@gmail.com>
2022-05-05 20:54:58 -0300

    operador unsigned

---
+++ aula26 copy/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+    O que é operador unsigned? [número sem sinal]
+*/
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+
+    
+
+    return 0;
+}
