2525a6134de0d1f2741e4a120e35f9d7f9405688
Izaac Baptista <izaacbaptista@gmail.com>
2022-05-11 15:08:51 -0300

    Como TROCAR os valores de duas variáveis

---
+++ aula29/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+    2) Elabore um algoritmo que receba, por meio do teclado, dois valores, um para a variável “a” e um para a variável “b”. 
+    Em seguida, faça os passos que julgar necessário para que ao final, a variável “a” possua o valor que inicialmente estava em “b” 
+    e a variável “b” possua o valor que inicialmente estava em “a”. 
+    Traduza seu algoritmo para a linguagem C e exiba os valores na tela.
+
+*/
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+    
+    int a, b;
+
+    printf("Digite dois valores: ");
+    scanf("%d%d", &a, &b);
+
+    printf("Valores lidos: a: %d\tb: %d\n", a, b);
+
+    printf("Valores lidos: a: %d\tb: %d\n", a, b);
+
+    
+
+    return 0;
+}
