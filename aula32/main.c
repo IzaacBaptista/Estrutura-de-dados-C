e8afecccd8039c00c9455a604ce84c2f34fdaf6c
Izaac Baptista <izaacbaptista@gmail.com>
2022-05-19 08:16:28 -0300

    Como calcular o valor a ser pago ao encanador

--- aula32/main.c
+++ aula32/main.c
@@ -3,23 +3,23 @@
 #include <locale.h>
 
 /*
-    Exercício Calculando gorjeta e dividindo conta de restaurante
+    Exercício Como calcular o valor a ser pago ao encanador
 */
 
 int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
-    
-    int numPessoas;
-    float valorDespesa, gorjeta, valorTotal, valorPorPessoa;
-    
-    printf("Digite o valor da conta, da gorjeta e a quantidade de pessoas: ");
-    scanf("%f%f%d", &valorDespesa, &gorjeta, &numPessoas);
-    
-    valorTotal = valorDespesa * gorjeta/100 + valorDespesa;
-    valorPorPessoa = valorTotal/numPessoas;
-    
-    printf("Cada um ira pagar R$%.2f\n", valorPorPessoa);
+
+        float dias, valorFinal, desconto;
+
+        printf("Quantos dias trabalhados? ");
+        scanf("%f", &dias);
+
+        valorFinal = dias * 45;
+        desconto = valorFinal * 8/100;
+        valorFinal = valorFinal - desconto;
+
+        printf("A receber: R$%.2f\nDesconto R$%.2f\n", valorFinal, desconto);
 
     return 0;
 }
