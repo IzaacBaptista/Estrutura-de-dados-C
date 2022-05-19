056e8eb0b254409ac2bff17f5b2b288764c4660e
Izaac Baptista <izaacbaptista@gmail.com>
2022-05-19 08:24:59 -0300

    Como converter valor em Real para Dólar

---
+++ aula33/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+    Exercício Como converter valor em Real para Dólar
+*/
+
+int main(void)
+{
+    setlocale(LC_ALL, "Portuguese");
+
+        
+
+    return 0;
+}
