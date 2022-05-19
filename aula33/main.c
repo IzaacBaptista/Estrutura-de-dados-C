a91c810dd97580fa04e781eaa817b1a72daf5597
Izaac Baptista <izaacbaptista@gmail.com>
2022-05-19 08:27:51 -0300

    Como converter valor em Real para Dólar

--- aula33/main.c
+++ aula33/main.c
@@ -10,7 +10,13 @@ int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
 
-        
+        float valorReal, valorDolar;
+
+        printf("Digite o valor em reais a ser convertido: ");
+        scanf("%f", & valorReal);
+
+        valorDolar = valorReal / 5.3;
+        printf("Valor em dolar $%.2f\n", valorDolar);
 
     return 0;
-}
+}a
