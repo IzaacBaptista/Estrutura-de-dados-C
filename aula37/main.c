5b3a87915dcdc2ca604a42b0cba7045de68ed3de
Izaac Baptista <izaacbaptista@gmail.com>
2022-06-10 08:09:10 -0300

    Verdadeiro e falso na linguagem C

---
+++ aula37/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+    Verdadeiro e falso na linguagem C
+*/
+
+int main() {
+
+    int a =10;
+
+    printf("Digite um valor qualquer: ");
+    scanf("%d", &a);
+
+    printf("\nResultado logico: %d\n", a < 0);
+
+    if( a < 0 )
+        printf("\n\tValor negativo!\n");
+    else {
+        printf("\n\tValor positivo!\n");
+    }
+
+    printf("Continuação do programa!\n");
+
+    return 0;
+}
