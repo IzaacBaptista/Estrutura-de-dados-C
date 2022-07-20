fb72a837745d3468f76ee2572139a884e5ba473a
Izaac Baptista <izaacbaptista@gmail.com>
2022-07-20 12:07:34 -0300

    O operador ternário na linguagem C

--- aula39/main.c
+++ aula39/main.c
@@ -14,6 +14,7 @@ int main() {
     printf("Digite um valor qualquer: ");
     scanf("%d", &a);
 
+    printf("Operador ternário");
     a < 0 ? printf("\n\tValor negativo!\n") : printf("\n\tValor positivo ou igual a zero!\n");
     
     printf("Continuação do programa!\n");
