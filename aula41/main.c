2710df81079f762e4a04132300b999775090918f
Restyled.io <commits@restyled.io>
2022-07-20 15:26:10 +0000

    Restyled by astyle

--- aula41/main.c
+++ aula41/main.c
@@ -17,18 +17,18 @@ int main() {
 
     printf("Operador ternário");
     a < 0 ? printf("\n\tValor negativo!\n") :
-        a > 0 ? printf("\n\tValor positivo!\n") : printf("\n\tValor igual a zero!\n");
-/*
-    printf("\n\nIf/else\n");
-    if(a < 0)
-        printf("\n\tValor negativo!\n");
-    else {
-        if(a > 0)
-            printf("\n\tValor positivo!\n");
-        else 
-            printf("\n\tValor igual a zero!\n");
-    }
-*/    
+    a > 0 ? printf("\n\tValor positivo!\n") : printf("\n\tValor igual a zero!\n");
+    /*
+        printf("\n\nIf/else\n");
+        if(a < 0)
+            printf("\n\tValor negativo!\n");
+        else {
+            if(a > 0)
+                printf("\n\tValor positivo!\n");
+            else
+                printf("\n\tValor igual a zero!\n");
+        }
+    */
     printf("Continuação do programa!\n");
 
     return 0;
