24f1cfec8c17167b059cde95ffe937704797a2b8
Restyled.io <commits@restyled.io>
2022-07-20 15:18:52 +0000

    Restyled by clang-format

--- aula39 copy/main.c
+++ aula39 copy/main.c
@@ -1,6 +1,6 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
     Decisão aninhadas na linguagem C [operador if/else aninhados]
@@ -8,27 +8,28 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    int a = 10;
+  int a = 10;
 
-    printf("Digite um valor qualquer: ");
-    scanf("%d", &a);
+  printf("Digite um valor qualquer: ");
+  scanf("%d", &a);
 
-    printf("Operador ternário");
-    a < 0 ? printf("\n\tValor negativo!\n") : printf("\n\tValor positivo ou igual a zero!\n");
+  printf("Operador ternário");
+  a < 0 ? printf("\n\tValor negativo!\n")
+        : printf("\n\tValor positivo ou igual a zero!\n");
 
-    printf("\n\nIf/else\n");
-    if(a < 0)
-        printf("\n\tValor negativo!\n");
-    else {
-        if(a > 0)
-            printf("\n\tValor positivo!\n");
-        else
-            printf("\n\tValor igual a zero!\n");
-    }
+  printf("\n\nIf/else\n");
+  if (a < 0)
+    printf("\n\tValor negativo!\n");
+  else {
+    if (a > 0)
+      printf("\n\tValor positivo!\n");
+    else
+      printf("\n\tValor igual a zero!\n");
+  }
 
-    printf("Continuação do programa!\n");
+  printf("Continuação do programa!\n");
 
-    return 0;
+  return 0;
 }
