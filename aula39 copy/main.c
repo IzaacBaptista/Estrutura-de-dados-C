2fc0ad356dcb649daeac37b61f0ab0585fc603c7
Izaac Baptista <izaacbaptista@gmail.com>
2022-07-20 12:17:47 -0300

    Decisão aninhadas na linguagem C [operador if/else aninhados]

---
+++ aula39 copy/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+    Decisão aninhadas na linguagem C [operador if/else aninhados]
+*/
+
+int main() {
+
+    setlocale(LC_ALL, "Portuguese");
+
+    int a = 10;
+
+    printf("Digite um valor qualquer: ");
+    scanf("%d", &a);
+
+    printf("Operador ternário");
+    a < 0 ? printf("\n\tValor negativo!\n") : printf("\n\tValor positivo ou igual a zero!\n");
+
+    printf("\n\nIf/else\n");
+    if(a < 0)
+        printf("\n\tValor negativo!\n");
+    else {
+        if(a > 0)
+            printf("\n\tValor positivo!\n");
+        else 
+            printf("\n\tValor igual a zero!\n");
+    }
+    
+    printf("Continuação do programa!\n");
+
+    return 0;
+}
