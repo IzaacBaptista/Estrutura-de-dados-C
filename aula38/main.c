29f3775d8fcc0e171711a5cc69da87f78588d833
Izaac Baptista <izaacbaptista@gmail.com>
2022-07-20 11:57:43 -0300

    Endentação e uso das chaves na linguagem C

---
+++ aula38/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+    Endentação e uso das chaves na linguagem C 
+*/
+
+int main() {
+
+    int a =10;
+
+    printf("Digite um valor qualquer: ");
+    scanf("%d", &a);
+
+    printf("\nResultado logico: %d\n", a < 0);
+
+    if(a < 0)
+        printf("\n\tValor negativo!\n");
+    else {
+        printf("\n\tValor positivo ou igual a zero!\n");
+        printf("\n\t**Valor positivo ou igual a zero!**\n");
+    }
+
+    printf("Continuação do programa!\n");
+
+    return 0;
+}
