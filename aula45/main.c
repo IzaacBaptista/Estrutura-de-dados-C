4971ec271c768c287867b40599e7906b50bfeb8e
Restyled.io <commits@restyled.io>
2022-07-21 16:49:16 +0000

    Restyled by clang-format

--- aula45/main.c
+++ aula45/main.c
@@ -1,6 +1,6 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
     Estrutura de decisão else if encadeados na linguagem C
@@ -8,30 +8,30 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    int a = 10;
+  int a = 10;
 
-    printf("\nDigite um valor qualquer: ");
-    scanf("%d", &a);
+  printf("\nDigite um valor qualquer: ");
+  scanf("%d", &a);
 
-    //primeira versão
-    if(a < 0)
-        printf("\n\tValor negativo!\n");
-    else {
-        if(a > 0)
-            printf("\n\tValor positivo!\n");
-        else
-            printf("\n\tValor igual a zero!\n");
-    }
-
-    //segunda versão
-    if(a < 0)
-        printf("\n\tValor negativo!\n");
-    else if(a > 0)
-        printf("\n\tValor positivo!\n");
+  // primeira versão
+  if (a < 0)
+    printf("\n\tValor negativo!\n");
+  else {
+    if (a > 0)
+      printf("\n\tValor positivo!\n");
     else
-        printf("\n\tValor igual a zero!\n");
+      printf("\n\tValor igual a zero!\n");
+  }
 
-    return 0;
+  // segunda versão
+  if (a < 0)
+    printf("\n\tValor negativo!\n");
+  else if (a > 0)
+    printf("\n\tValor positivo!\n");
+  else
+    printf("\n\tValor igual a zero!\n");
+
+  return 0;
 }
