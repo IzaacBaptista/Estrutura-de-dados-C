d8ec5b6a4b5b7bd3fe29119a95d9b37b19bbef50
Restyled.io <commits@restyled.io>
2022-07-21 16:20:23 +0000

    Restyled by clang-format

--- aula42/main.c
+++ aula42/main.c
@@ -1,6 +1,6 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
     O operador lógico de negação ! na linguagem C
@@ -8,28 +8,29 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    int a = 10;
+  int a = 10;
 
-    printf("Digite um valor qualquer: ");
-    scanf("%d", &a);
+  printf("Digite um valor qualquer: ");
+  scanf("%d", &a);
 
-    printf("Operador ternário");
-    !(a < 0) ? printf("\n\tValor positivo!\n") :
-    a > 0 ? printf("\n\tValor negativo!\n") : printf("\n\tValor igual a zero!\n");
+  printf("Operador ternário");
+  !(a < 0) ? printf("\n\tValor positivo!\n")
+  : a > 0  ? printf("\n\tValor negativo!\n")
+           : printf("\n\tValor igual a zero!\n");
 
-    printf("\n\nIf/else\n");
-    if(!a < 0)
-        printf("\n\tValor negativo!\n");
-    else {
-        if(a > 0)
-            printf("\n\tValor positivo!\n");
-        else
-            printf("\n\tValor igual a zero!\n");
-    }
+  printf("\n\nIf/else\n");
+  if (!a < 0)
+    printf("\n\tValor negativo!\n");
+  else {
+    if (a > 0)
+      printf("\n\tValor positivo!\n");
+    else
+      printf("\n\tValor igual a zero!\n");
+  }
 
-    printf("Continuação do programa!\n");
+  printf("Continuação do programa!\n");
 
-    return 0;
+  return 0;
 }
