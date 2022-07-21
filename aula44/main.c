d6ca1ef64f5649af8844730e6b2a96059726ae93
Izaac Baptista <izaacbaptista@gmail.com>
2022-07-21 13:38:05 -0300

    O operador lógico ou (||) na linguagem C (Disjunção)

---
+++ aula44/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+    O operador lógico ou (||) na linguagem C (Disjunção) 
+*/
+
+int main() {
+
+    setlocale(LC_ALL, "Portuguese");
+
+/*    int idade;
+    
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    if(idade menor ou igual a 5){
+        printf("Este passageiro tem direito à gratuidade!");
+    else{
+        if(idade maior ou igual a 60)
+        printf("Este passageiro tem direito à gratuidade!");
+        else
+        printf("Este passageiro não tem direito à gratuidade!");
+    }
+*/
+    //Se a idade do passageiro for menor ou igual a 5 ou maior ou igual a 60 o passageiro possui gratuidade.
+
+    //Contudo, esta versão pode ser melhorada utilizando o operador lógico ou, assim:
+
+    int idade;
+    
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    if(idade menor ou igual 5 || idade maior ou igual 60)
+        printf("Este passageiro tem direito à gratuidade!");
+    else
+        printf("Este passageiro não tem direito à gratuidade!");
+
+    return 0;
+}
