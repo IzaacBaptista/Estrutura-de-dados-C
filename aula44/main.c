aa280481919062ba54ef25ada5a1e5e5e3b6ef02
Restyled.io <commits@restyled.io>
2022-07-21 16:38:16 +0000

    Restyled by clang-format

--- aula44/main.c
+++ aula44/main.c
@@ -1,6 +1,6 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
     O operador lógico ou (||) na linguagem C (Disjunção)
@@ -8,35 +8,37 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    /*    int idade;
+  /*    int idade;
 
-        printf("Digite sua idade: ");
-        scanf("%d", &idade);
+      printf("Digite sua idade: ");
+      scanf("%d", &idade);
 
-        if(idade menor ou igual a 5){
-            printf("Este passageiro tem direito à gratuidade!");
-        else{
-            if(idade maior ou igual a 60)
-            printf("Este passageiro tem direito à gratuidade!");
-            else
-            printf("Este passageiro não tem direito à gratuidade!");
-        }
-    */
-    //Se a idade do passageiro for menor ou igual a 5 ou maior ou igual a 60 o passageiro possui gratuidade.
+      if(idade menor ou igual a 5){
+          printf("Este passageiro tem direito à gratuidade!");
+      else{
+          if(idade maior ou igual a 60)
+          printf("Este passageiro tem direito à gratuidade!");
+          else
+          printf("Este passageiro não tem direito à gratuidade!");
+      }
+  */
+  // Se a idade do passageiro for menor ou igual a 5 ou maior ou igual a 60 o
+  // passageiro possui gratuidade.
 
-    //Contudo, esta versão pode ser melhorada utilizando o operador lógico ou, assim:
+  // Contudo, esta versão pode ser melhorada utilizando o operador lógico ou,
+  // assim:
 
-    int idade;
+  int idade;
 
-    printf("Digite sua idade: ");
-    scanf("%d", &idade);
+  printf("Digite sua idade: ");
+  scanf("%d", &idade);
 
-    if(idade menor ou igual 5 || idade maior ou igual 60)
-        printf("Este passageiro tem direito à gratuidade!");
-    else
-        printf("Este passageiro não tem direito à gratuidade!");
+  if (idade menor ou igual 5 || idade maior ou igual 60)
+    printf("Este passageiro tem direito à gratuidade!");
+  else
+    printf("Este passageiro não tem direito à gratuidade!");
 
-    return 0;
+  return 0;
 }
