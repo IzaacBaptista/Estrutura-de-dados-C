a6dcdcf58c8682990f9fb2afe3a6f7ed9157a6da
Restyled.io <commits@restyled.io>
2022-07-26 16:09:37 +0000

    Restyled by clang-format

--- aula47/main.c
+++ aula47/main.c
@@ -1,6 +1,6 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
     Estrutura de seleção switch case com caracter na linguagem C
@@ -8,33 +8,33 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    int opcap;
+  int opcap;
 
-    printf("a - Cadastrar produto\nb - Vender produto\nc - Buscar produto\nd - Imprimir relatório\ne - Sair");
-    scanf("%d", &opcao);
+  printf("a - Cadastrar produto\nb - Vender produto\nc - Buscar produto\nd - "
+         "Imprimir relatório\ne - Sair");
+  scanf("%d", &opcao);
 
-    switch(opcao) {
-    case 'a':
-        printf("a - Cadastrar produto\n");
-        break;
-    case 'b':
-        printf("b - Vender produto\n");
-        break;
-    case 'c':
-        printf("c - Buscar produto\n");
-        break;
-    case 'd':
-        printf("d - Imprimir relatório\n");
-        break;
-    case 'e':
-        printf("e - Sair\n");
-        break;
-    default:
-        printf("opção não encontrada, tente novamente!");
-    }
+  switch (opcao) {
+  case 'a':
+    printf("a - Cadastrar produto\n");
+    break;
+  case 'b':
+    printf("b - Vender produto\n");
+    break;
+  case 'c':
+    printf("c - Buscar produto\n");
+    break;
+  case 'd':
+    printf("d - Imprimir relatório\n");
+    break;
+  case 'e':
+    printf("e - Sair\n");
+    break;
+  default:
+    printf("opção não encontrada, tente novamente!");
+  }
 
-
-    return 0;
+  return 0;
 }
