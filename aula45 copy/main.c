0d8daf29e9cde62cc2c70442b47df674ea58f3db
Izaac Baptista <izaacbaptista@gmail.com>
2022-07-26 12:56:11 -0300

    Estrutura de seleção switch case

---
+++ aula45 copy/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+    Estrutura de seleção switch case na linguagem C 
+*/
+
+int main() {
+
+    setlocale(LC_ALL, "Portuguese");
+
+    
+
+    return 0;
+}
