acccfeb48b0f0029a4ba92bb14f1ec2ef0f512ce
Restyled.io <commits@restyled.io>
2022-07-26 16:09:35 +0000

    Restyled by astyle

--- aula47/main.c
+++ aula47/main.c
@@ -3,7 +3,7 @@
 #include <locale.h>
 
 /*
-    Estrutura de seleção switch case com caracter na linguagem C 
+    Estrutura de seleção switch case com caracter na linguagem C
 */
 
 int main() {
@@ -15,7 +15,7 @@ int main() {
     printf("a - Cadastrar produto\nb - Vender produto\nc - Buscar produto\nd - Imprimir relatório\ne - Sair");
     scanf("%d", &opcao);
 
-    switch(opcao){
+    switch(opcao) {
     case 'a':
         printf("a - Cadastrar produto\n");
         break;
@@ -34,7 +34,7 @@ int main() {
     default:
         printf("opção não encontrada, tente novamente!");
     }
-    
+
 
     return 0;
 }
