52240ee8abe1ef6bec2eaf7dac60e555a49756a4
Izaac Baptista <izaacbaptista@gmail.com>
2022-07-28 15:27:49 -0300

    Exercício 2: Contando números positivos e negativos

--- Intermediário/Exercícios/exercicio2.c
+++ Intermediário/Exercícios/exercicio2.c
@@ -10,47 +10,49 @@ int main() {
 
     setlocale(LC_ALL, "Portuguese");
 
-    int um, dois, tres, quatro, cinco, positivos, negativos;
+    int valor, positivos, negativos;
 
     printf("Digite o primeiro valor:");
-    scanf("%d", &um);
+    scanf("%d", &valor);
 
-    printf("Digite o primeiro valor:");
-    scanf("%d", &dois);
-
-    printf("Digite o primeiro valor:");
-    scanf("%d", &tres);
-
-    printf("Digite o primeiro valor:");
-    scanf("%d", &quatro);
-
-    printf("Digite o primeiro valor:");
-    scanf("%d", &cinco);
-
-    if(um < 0)
+    if(valor < 0)
         negativos++;
     else
         positivos++;
 
-    if(dois < 0)
+    printf("Digite o segundo valor:");
+    scanf("%d", &valor);
+
+    if(valor < 0)
         negativos++;
     else
         positivos++;
 
-    if(tres < 0)
+    printf("Digite o terceiro valor:");
+    scanf("%d", &valor);
+
+    if(valor < 0)
         negativos++;
     else
         positivos++;
 
-    if(quatro < 0)
+    printf("Digite o quarto valor:");
+    scanf("%d", &valor);
+
+    if(valor < 0)
         negativos++;
     else
         positivos++;
 
-    if(cinco < 0)
+    printf("Digite o quinto valor:");
+    scanf("%d", &valor);
+
+    if(valor < 0)
         negativos++;
     else
         positivos++;
+
+    printf("\nPositivos: %d\nNegativos: %d\n", positivos, negativos);
 
     return 0;
 }
