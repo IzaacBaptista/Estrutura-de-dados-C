2f1f3cf2249b182f83f6eff822eb0690fe4a0249
Restyled.io <commits@restyled.io>
2022-08-05 15:03:15 +0000

    Restyled by clang-format

--- Intermediário/Exercícios/exercicio3.c
+++ Intermediário/Exercícios/exercicio3.c
@@ -1,24 +1,25 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
-     Ex 3 Leia um número e diga se é divisível por 2, por 3 ou por 5 na linguagem C
+     Ex 3 Leia um número e diga se é divisível por 2, por 3 ou por 5 na
+   linguagem C
 */
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    int num;
+  int num;
 
-    printf("Digite um valor qualquer: ");
-    scanf("%d", &num);
+  printf("Digite um valor qualquer: ");
+  scanf("%d", &num);
 
-    if(num % 2 == 0 || num % 3 == 0 || num % 5 == 0)
-        printf("É divisível por 2, 3 ou 5.\n");
-    else
-        printf("Não é divisível por 2, 3 ou 5.\n");
+  if (num % 2 == 0 || num % 3 == 0 || num % 5 == 0)
+    printf("É divisível por 2, 3 ou 5.\n");
+  else
+    printf("Não é divisível por 2, 3 ou 5.\n");
 
-    return 0;
+  return 0;
 }
