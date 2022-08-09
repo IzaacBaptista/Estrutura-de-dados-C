94865f4bd145ff12ec4567e2cfabad98a4dfb2ac
Restyled.io <commits@restyled.io>
2022-08-09 15:04:20 +0000

    Restyled by clang-format

--- Intermediário/Exercícios/exercicio5.c
+++ Intermediário/Exercícios/exercicio5.c
@@ -1,6 +1,6 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
     Exercício 5: Calcular o índice de massa corporal (IMC) na linguagem C
@@ -8,27 +8,27 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    float massa, altura, imc;
+  float massa, altura, imc;
 
-    printf("Digite seu peso (em kg) e sua altura (em metros): ");
-    scanf("%f%f", &massa, &altura);
+  printf("Digite seu peso (em kg) e sua altura (em metros): ");
+  scanf("%f%f", &massa, &altura);
 
-    imc = massa / (altura * altura);
+  imc = massa / (altura * altura);
 
-    if(imc < 18.5)
-        printf("IMC: %f\tAbaixo do peso\n", imc);
-    else if(imc >= 18.5 && imc < 25)
-        printf("IMC: %f\tPeso normal\n", imc);
-    else if(imc >= 25 && imc < 30)
-        printf("IMC: %f\tSobrepeso\n", imc);
-    else if(imc >= 30 && imc < 35)
-        printf("IMC: %f\tObesidade grau 1\n", imc);
-    else if(imc >= 35 && imc < 40)
-        printf("IMC: %f\tObesidade grau 2\n", imc);
-    else
-        printf("IMC: %f\tObesidade grau 3\n", imc);
+  if (imc < 18.5)
+    printf("IMC: %f\tAbaixo do peso\n", imc);
+  else if (imc >= 18.5 && imc < 25)
+    printf("IMC: %f\tPeso normal\n", imc);
+  else if (imc >= 25 && imc < 30)
+    printf("IMC: %f\tSobrepeso\n", imc);
+  else if (imc >= 30 && imc < 35)
+    printf("IMC: %f\tObesidade grau 1\n", imc);
+  else if (imc >= 35 && imc < 40)
+    printf("IMC: %f\tObesidade grau 2\n", imc);
+  else
+    printf("IMC: %f\tObesidade grau 3\n", imc);
 
-    return 0;
+  return 0;
 }
