b2d6c2aa845773d353c36893a39792389ac99d83
Restyled.io <commits@restyled.io>
2022-08-17 14:50:43 +0000

    Restyled by clang-format

--- Intermediário/Exercícios/exercicio7.c
+++ Intermediário/Exercícios/exercicio7.c
@@ -1,6 +1,6 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
     Leia um caractere e diga se é vogal ou consoante na linguagem C
@@ -8,24 +8,26 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    char letra;
+  char letra;
 
-    printf("Digite um caracter: ");
-    scanf("%d", &letra);
+  printf("Digite um caracter: ");
+  scanf("%d", &letra);
 
-    if(letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u') {
-        printf("caractere digitado é uma vogal");
-    } else {
-        printf("caractere digitado é uma consoante");
-    }
+  if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' ||
+      letra == 'u') {
+    printf("caractere digitado é uma vogal");
+  } else {
+    printf("caractere digitado é uma consoante");
+  }
 
-    if(letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') {
-        printf("caractere digitado é uma vogal");
-    } else {
-        printf("caractere digitado é uma consoante");
-    }
+  if (letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' ||
+      letra == 'U') {
+    printf("caractere digitado é uma vogal");
+  } else {
+    printf("caractere digitado é uma consoante");
+  }
 
-    return 0;
+  return 0;
 }
