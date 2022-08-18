83c98272080a8fa1bee81953c44af02bbe02e03b
Restyled.io <commits@restyled.io>
2022-08-18 15:10:06 +0000

    Restyled by clang-format

--- Intermediário/Exercícios/exercicio8.c
+++ Intermediário/Exercícios/exercicio8.c
@@ -1,42 +1,43 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
-    Dado o número do mês, diga quantos dias têm esse mês (versão 2) na linguagem C
+    Dado o número do mês, diga quantos dias têm esse mês (versão 2) na linguagem
+   C
 */
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    int mes;
+  int mes;
 
-    printf("Digite um numero: ");
-    scanf("%d", &mes);
+  printf("Digite um numero: ");
+  scanf("%d", &mes);
 
-    switch(mes) {
-    case 1:
-    case 3:
-    case 5:
-    case 7:
-    case 8:
-    case 10:
-    case 12:
-        printf("\nO mes possui 31 dias\n");
-        break;
-    case 2:
-        printf("\nO mes possui 28 dias\n");
-        break;
-    case 4:
-    case 6:
-    case 9:
-    case 11:
-        printf("\nO mes possui 30 dias\n");
-        break;
-    default:
-        printf("\nValor nao corresponde a nenhum mes!\n");
-    }
+  switch (mes) {
+  case 1:
+  case 3:
+  case 5:
+  case 7:
+  case 8:
+  case 10:
+  case 12:
+    printf("\nO mes possui 31 dias\n");
+    break;
+  case 2:
+    printf("\nO mes possui 28 dias\n");
+    break;
+  case 4:
+  case 6:
+  case 9:
+  case 11:
+    printf("\nO mes possui 30 dias\n");
+    break;
+  default:
+    printf("\nValor nao corresponde a nenhum mes!\n");
+  }
 
-    return 0;
+  return 0;
 }
