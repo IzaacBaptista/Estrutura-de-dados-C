b69608f13383a5b8f853bd1c7c6d68eaf4a5692f
Restyled.io <commits@restyled.io>
2022-08-24 16:24:08 +0000

    Restyled by astyle

--- Intermediário/Exercícios/exercicio10.c
+++ Intermediário/Exercícios/exercicio10.c
@@ -3,7 +3,7 @@
 #include <locale.h>
 
 /*
-    Como calcular a média aritmética e ponderada (versão 2) na linguagem C 
+    Como calcular a média aritmética e ponderada (versão 2) na linguagem C
 */
 
 int main() {
@@ -16,18 +16,17 @@ int main() {
     printf("Qual a media deseja?\na - aritmetica\np - ponderada\n");
     scanf("%c", &escolha);
 
-    if(escolha == 'a' || escolha == 'p'){
+    if(escolha == 'a' || escolha == 'p') {
         printf("Digite as tres notas: ");
         scanf("%f%f%f", &nota1, &nota2, &nota3);
     }
 
     if(escolha == 'a')
         printf("Media aritmetica: %.2f\n", (nota1 + nota2 + nota3) / 3);
+    else if(escolha == 'p')
+        printf("Media ponderada: %.2f\n", (nota1 * 3 + nota2 * 3 + nota3 * 4) / 10);
     else
-        if(escolha == 'p')
-            printf("Media ponderada: %.2f\n", (nota1 * 3 + nota2 * 3 + nota3 * 4) / 10);
-        else
-            printf("\nOpcao invalida!!!\n");
-    
+        printf("\nOpcao invalida!!!\n");
+
     return 0;
 }
