fe27c20f73611fbad91d7a049da1c34577771023
Restyled.io <commits@restyled.io>
2022-08-25 16:17:52 +0000

    Restyled by clang-format

--- Intermediário/Exercícios/exercicio11.c
+++ Intermediário/Exercícios/exercicio11.c
@@ -1,6 +1,6 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
     Como calcular a média aritmética e ponderada (versão 2) na linguagem C
@@ -8,24 +8,23 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    int a, b, c;
+  int a, b, c;
 
-    printf("Digite tres valores: ");
-    scanf("%d%d%d", &a, &b, &c);
+  printf("Digite tres valores: ");
+  scanf("%d%d%d", &a, &b, &c);
 
-    if(a + b > c && a + c > b && b + c > a) {
-        printf("Os 3 lados formam um triangulo!\n");
-        if(a == b && a == c)
-            printf("Equilatero\n");
-        else if(a == b || a == c || b == c)
-            printf("Isosceles\n");
-        else
-            printf("Escaleno\n");
-    }
+  if (a + b > c && a + c > b && b + c > a) {
+    printf("Os 3 lados formam um triangulo!\n");
+    if (a == b && a == c)
+      printf("Equilatero\n");
+    else if (a == b || a == c || b == c)
+      printf("Isosceles\n");
     else
-        printf("Os 3 lados NAO formam um trinagulo!\n");
+      printf("Escaleno\n");
+  } else
+    printf("Os 3 lados NAO formam um trinagulo!\n");
 
-    return 0;
+  return 0;
 }
