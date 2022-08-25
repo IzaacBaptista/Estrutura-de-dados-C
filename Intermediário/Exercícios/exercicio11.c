fec4f8db1a428c538808f2e654230d5c62811d4e
Izaac Baptista <izaacbaptista@gmail.com>
2022-08-25 13:17:41 -0300

    Três valores formam um triângulo equilátero, isósceles ou escaleno

---
+++ Intermediário/Exercícios/exercicio11.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+/*
+    Como calcular a média aritmética e ponderada (versão 2) na linguagem C 
+*/
+
+int main() {
+
+    setlocale(LC_ALL, "Portuguese");
+
+    int a, b, c;
+
+    printf("Digite tres valores: ");
+    scanf("%d%d%d", &a, &b, &c);
+
+    if(a + b > c && a + c > b && b + c > a){
+        printf("Os 3 lados formam um triangulo!\n");
+        if(a == b && a == c)
+            printf("Equilatero\n");
+        else
+            if(a == b || a == c || b == c)
+                printf("Isosceles\n");
+            else
+                printf("Escaleno\n");
+    }
+    else
+        printf("Os 3 lados NAO formam um trinagulo!\n");
+
+    return 0;
+}
