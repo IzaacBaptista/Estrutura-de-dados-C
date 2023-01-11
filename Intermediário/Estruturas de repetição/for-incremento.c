f0ff0ec763130f754b41a9ea5010019dfe8aa451
Restyled.io <commits@restyled.io>
2023-01-11 15:21:15 +0000

    Restyled by astyle

--- Intermediário/Estruturas de repetição/for-incremento.c
+++ Intermediário/Estruturas de repetição/for-incremento.c
@@ -3,20 +3,20 @@
 #include <locale.h>
 
 /*
-    Estrutura de repetição for com incremento diferente de 1 na linguagem C 
+    Estrutura de repetição for com incremento diferente de 1 na linguagem C
 */
 
 int main() {
 
     setlocale(LC_ALL, "Portuguese");
 
-   int i;
+    int i;
 
-   for(i = 1; i <= 100; i+=2{
-        if(i % 2 != 0){
+    for(i = 1; i <= 100; i+=2{
+    if(i % 2 != 0) {
             printf("%d ", i);
         }
-   }
+    }
 
     return 0;
 }
