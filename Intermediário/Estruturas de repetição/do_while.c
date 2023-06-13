a03222af4b7b33d78b1df60d719f427fe2958af5
Restyled.io <commits@restyled.io>
2023-06-13 15:13:22 +0000

    Restyled by clang-format

--- Intermediário/Estruturas de repetição/do_while.c
+++ Intermediário/Estruturas de repetição/do_while.c
@@ -1,6 +1,6 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
     Estrutura de repetição do while (faça enquanto) na linguagem C
@@ -8,27 +8,28 @@
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    int valor, valor2;
+  int valor, valor2;
 
-    printf("Digite um valor maior que zero: ");
+  printf("Digite um valor maior que zero: ");
+  scanf("%d", &valor);
+
+  while (valor <= 0) { // enquanto valor for menor ou igual a zero
+    printf("Valor invalido. Digite um valor maior que zero: ");
     scanf("%d", &valor);
+  }
 
-    while(valor <= 0) { // enquanto valor for menor ou igual a zero
-        printf("Valor invalido. Digite um valor maior que zero: ");
-        scanf("%d", &valor);
-    }
+  printf("Valor lido: %d\n\n", valor);
 
-    printf("Valor lido: %d\n\n", valor);
+  do
+    ( // faça
+        printf("Digite um valor maior que zero: ");
+        scanf("%d",
+              &valor2);) while (valor2 <=
+                                0); // enquanto valor for menor ou igual a zero
 
-    do( // faça
-            printf("Digite um valor maior que zero: ");
-            scanf("%d", &valor2);
-        )while(valor2 <= 0); // enquanto valor for menor ou igual a zero
+  printf("Valor lido: %d\n", valor2);
 
-    printf("Valor lido: %d\n", valor2);
-
-
-    return 0;
+  return 0;
 }
