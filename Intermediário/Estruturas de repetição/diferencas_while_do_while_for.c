ba56ff812576f1669f551af5e94e83e67bdc9bde
Restyled.io <commits@restyled.io>
2023-06-14 15:20:20 +0000

    Restyled by clang-format

--- Intermediário/Estruturas de repetição/diferencas_while_do_while_for.c
+++ Intermediário/Estruturas de repetição/diferencas_while_do_while_for.c
@@ -1,34 +1,35 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 
 /*
-    Diferença entre as estruturas de repetição for, while e do while na linguagem C
+    Diferença entre as estruturas de repetição for, while e do while na
+   linguagem C
 */
 
 int main() {
 
-    setlocale(LC_ALL, "Portuguese");
+  setlocale(LC_ALL, "Portuguese");
 
-    //for
-    int i = 0;
-    for(i = 0; i < 10; i++) {
-        printf("Valor de i: %d\n", i);
-    }
+  // for
+  int i = 0;
+  for (i = 0; i < 10; i++) {
+    printf("Valor de i: %d\n", i);
+  }
 
-    //while
-    int i = 0;
-    while(i < 10) {
-        printf("Valor de i: %d\n", i);
-        i++;
-    }
+  // while
+  int i = 0;
+  while (i < 10) {
+    printf("Valor de i: %d\n", i);
+    i++;
+  }
 
-    //do while
-    int i = 0;
-    do {
-        printf("Valor de i: %d\n", i);
-        i++;
-    } while(i < 10);
+  // do while
+  int i = 0;
+  do {
+    printf("Valor de i: %d\n", i);
+    i++;
+  } while (i < 10);
 
-    return 0;
+  return 0;
 }
